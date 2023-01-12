ab0da09074fb60b49d30605bc75acb789cc71b49
ThunderWay <114352471+ThunderWay@users.noreply.github.com>
2023-01-12 10:20:15 +0300

    Update Source.cpp

--- Laba 2/Laba 2/Source.cpp
+++ Laba 2/Laba 2/Source.cpp
@@ -8,14 +8,14 @@ int main()
     SetConsoleOutputCP(1251);
     double Var, Var2, Var3, Var4, Var5, Var6, Var7;
 
-    cout << "Ââåä³òü 3 ÷èñëà: ";
+    cout << "Введіть 3 числа: ";
     cin >> Var >> Var2 >> Var3;
     Var4 = Var + Var2 + Var3;
     Var5 = Var * Var2 * Var3;
     Var6 = (Var + Var2 + Var3) / 3;
 
 
-    cout << "Ñóìà: " << Var4 << "\n\Äîáóòîê: " << Var5 << "\n\Середнє арифметичне: " << Var6;
+    cout << "Сума: " << Var4 << "\n\Добуток: " << Var5 << "\n\Середнє арифметичне: " << Var6;
 
     return _getch();
 }
